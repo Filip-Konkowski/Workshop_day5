b086b89ca7bc722a7db9fdf5475503c51bbafda0
Filip-Konkowski <fkonkowski@gmail.com>
2015-09-25 10:39:25 +0200

    added PHP and changes in HTML

--- napisz_hello_world/napisz_hello_world.php
+++ napisz_hello_world/napisz_hello_world.php
@@ -5,8 +5,15 @@
  * Date: 25.09.15
  * Time: 10:15
  */
-
-
+$imieDostepu = "Filip";
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST["login"])){
+        $imie = trim($_POST["login"]);
+        if ($imie == $imieDostepu){
+            $wyswietl = ("Cześć " . $imie);
+        }
+    }
+}
 
 ?>
 
@@ -17,13 +24,13 @@
     <title>Formularz</title>
 </head>
 <body>
-    <form action="login.php" method="POST">
+    <form action="napisz_hello_world.php" method="POST">
         <p>
             Imię Użytkownika:
             <input type="text" name="login" >
         </p>
         <input type="submit" value="wyślij">
     </form>
-
+    <p><?php echo $wyswietl?$wyswietl: ""  ?></p>
 </body>
 </html>
