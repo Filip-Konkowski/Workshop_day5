4d77c86bc8dae0d228f9141b8f04a615194ee1a0
Filip-Konkowski <fkonkowski@gmail.com>
2015-09-25 10:22:53 +0200

    add file napisz_hello_world.php with html lines

---
+++ napisz_hello_world/napisz_hello_world.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 25.09.15
+ * Time: 10:15
+ */
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="utf-8">
+    <title>Formularz</title>
+</head>
+<body>
+    <form action="login.php" method="POST">
+        <p>
+            Imię Użytkownika:
+            <input type="text" name="login" >
+        </p>
+        <input type="submit" value="wyślij">
+    </form>
+
+</body>
+</html>
